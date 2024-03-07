983feb83f0dfd27782f5f7f106ec0eb5dd6be57c
Subrat Kumar Behera <subratkumarbehera1991@gmail.com>
2024-03-07 21:00:42 +0530

    cdn react create element

--- 01-react-cdn/main.js
+++ 01-react-cdn/main.js
@@ -1,2 +1,9 @@
+const GoogleButton = React.createElement(
+  "h1",
+  { className: "greeting" },
+  "Hello"
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>hello World</h1>);
+// root.render(<h1>hello World</h1>);
+root.render(GoogleButton);

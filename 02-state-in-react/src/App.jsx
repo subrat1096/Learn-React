8c9d5478b9fcd3dd1297c6f2bcf38f5d85fcae6c
Subrat Kumar Behera <subratkumarbehera1991@gmail.com>
2024-03-08 06:51:32 +0530

    Counter Project

---
+++ 02-state-in-react/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const IncrementValue = () => {
+    setCount(count + 1);
+  };
+  const DecrementValue = () => {
+    if (count === 0) return;
+    setCount(count - 1);
+  };
+  const Reset = () => {
+    setCount(0);
+  };
+  return (
+    <>
+      <h1 className="text-3xl font-bold">State Change In React</h1>
+      <h2 className="text-2xl font-bold my-4">Counter Value: {count}</h2>
+      <div className="flex justify-center gap-2 text-xl font-semibold items-center my-4">
+        <button
+          type="button"
+          className=" bg-black text-white px-4 py-2 rounded-md"
+          onClick={DecrementValue}
+        >
+          -
+        </button>
+        <button
+          type="button"
+          className=" bg-black text-white px-4 py-2 rounded-md"
+          onClick={Reset}
+        >
+          Reset
+        </button>
+        <button
+          type="button"
+          className=" bg-black text-white px-4 py-2 rounded-md"
+          onClick={IncrementValue}
+        >
+          +
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;

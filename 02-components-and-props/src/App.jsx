45b2740dee075b1e370d52eca868e0721fc2d0ab
Subrat Kumar Behera <subratkumarbehera1991@gmail.com>
2024-03-08 16:26:03 +0530

    passing object as props

--- 02-components-and-props/src/App.jsx
+++ 02-components-and-props/src/App.jsx
@@ -2,16 +2,29 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  let myObj = {
+    name: "Subrat Kumar Behera",
+    age: 32,
+    address: {
+      street: "Mahaveer Lane, Near U.P.School Badambadi",
+      "P.O": "Arunodaya Market",
+      "P.S": "Badambadi",
+      city: "Cuttack",
+      state: "Odisha",
+      country: "India",
+    },
+    description: `I'm a full-stack web developer. I am a senior programmer with
+    good knowledge of fron-end adnd back-end techniques.`,
+  };
   return (
     <>
       <h1 className="text-3xl font-bold bg-orange-500 p-3 rounded-lg">
         Vite With Tailwind CSS
       </h1>
       <Card
-        name={"Subrat Kumar Behera"}
+        name={myObj.name}
         btnTxt="View Profile →"
-        description={`I'm a full-stack web developer. I am a senior programmer with
-          good knowledge of fron-end adnd back-end techniques.`}
+        description={myObj.description}
       />
       <Card
         name={"My Journey"}

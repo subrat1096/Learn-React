0a0dcafc7d70e8d1a80542142560bf5021b363bc
Subrat Kumar Behera <subratkumarbehera1991@gmail.com>
2024-03-08 12:58:45 +0530

    passing props in react

--- 02-components-and-props/src/App.jsx
+++ 02-components-and-props/src/App.jsx
@@ -7,7 +7,24 @@ function App() {
       <h1 className="text-3xl font-bold bg-orange-500 p-3 rounded-lg">
         Vite With Tailwind CSS
       </h1>
-      <Card />
+      <Card
+        name={"Subrat Kumar Behera"}
+        btnTxt="View Profile →"
+        description={`I'm a full-stack web developer. I am a senior programmer with
+          good knowledge of fron-end adnd back-end techniques.`}
+      />
+      <Card
+        name={"My Journey"}
+        btnTxt="View My Journey →"
+        description={`I'm a full-stack web developer. I am a senior programmer with
+          good knowledge of fron-end adnd back-end techniques.`}
+      />
+      <Card
+        name={"Projects"}
+        btnTxt="View Projects →"
+        description={`I'm a full-stack web developer. I am a senior programmer with
+          good knowledge of fron-end adnd back-end techniques.`}
+      />
     </>
   );
 }
--- 02-components-and-props/src/components/Card.jsx
+++ 02-components-and-props/src/components/Card.jsx
@@ -1,5 +1,7 @@
+import PropTypes from "prop-types";
 import Subrat from "../assets/IMG_20211226_222138306.jpg";
-const Card = () => {
+
+const Card = ({ name, btnTxt, description }) => {
   return (
     <div className="relative h-[400px] w-[300px] rounded-md my-4">
       <img
@@ -9,19 +11,18 @@ const Card = () => {
       />
       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
       <div className="absolute bottom-4 left-4 text-left">
-        <h1 className="text-lg font-semibold text-white">
-          Subrat Kumar Behera
-        </h1>
-        <p className="mt-2 text-sm text-gray-300">
-          I&apos;m a full-stack web developer. I am a senior programmer with
-          good knowledge of fron-end adnd back-end techniques.
-        </p>
+        <h1 className="text-lg font-semibold text-white">{name}</h1>
+        <p className="mt-2 text-sm text-gray-300">{description}</p>
         <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-          View Profile →
+          {btnTxt}
         </button>
       </div>
     </div>
   );
 };
-
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  btnTxt: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 export default Card;
